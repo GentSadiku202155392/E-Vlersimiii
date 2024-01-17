2da6d63008d7f7301b02282530115a2518eacead
AlisaMa1 <alisamaliqi2002@gmail.com>
2024-01-17 23:01:13 +0100

    Controllers(Nota1, Nota2, Nota3, VleresimiPerfundimtar)

---
+++ E-Vlersimiii/E-Vlersimiii/Controllers/Nota2.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using E_Vlersimiii.Models;
+using Microsoft.EntityFrameworkCore;
+using E_Vlersimiii.Data;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Nota2Controller : ControllerBase
+{
+    private readonly E_VleresimiiContext _context;
+
+    public object?[]? NotaP2 { get; private set; }
+
+    public Nota2Controller(E_VleresimiiContext context)
+    {
+        _context = context;
+    }
+
+    // Get all Nota2
+    [HttpGet("GetNotaP2")]
+    public async Task<ActionResult<List<Nota2>>> Get()
+    {
+        return Ok(await _context.Nota2s.ToListAsync());
+    }
+
+    [HttpGet("{NotaP2}")]
+    // GET: api/Shippingschedules/ID
+
+    public async Task<ActionResult<Nota2>> GetNota1(int NotaP2)
+    {
+        ActionResult<Nota2> Nota2 = await _context.Nota2s.FindAsync(NotaP2);
+
+        if (NotaP2 == null)
+        {
+            return NotFound();
+        }
+
+        return Nota2;
+    }
+
+
+    // Create Nota2 
+    [HttpPost("ShtoNota2")]
+    public async Task<ActionResult<List<Nota2>>> AddNota2(Nota2 nota2)
+    {
+        _context.Nota2s.Add(nota2);
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.Nota2s.ToListAsync());
+    }
+
+    // Update Nota2
+    [HttpPut("UpdateNota2")]
+    public async Task<ActionResult<Nota2>> UpdateNota2(Nota2 request)
+    {
+        var dbNota2s = await _context.Nota2s.FindAsync(request.NotaP2);
+        if (dbNota2s == null)
+            return NotFound("Nota2 not found");
+
+        // Your update logic here
+
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.Nota2s.ToListAsync());
+    }
+
+    // Delete a ditar
+    [HttpDelete("FshijeNota2/{NotaP2}")]
+    public async Task<ActionResult<List<Nota2>>> DeleteNota2(int NotaP2)
+    {
+        var dbNota2s = await _context.Nota2s.FindAsync(NotaP2);
+        if (dbNota2s == null)
+            return NotFound("Nota2 not found");
+
+        Nota1 dbNota2 = null;
+        _context.Nota2s.Remove(dbNota2s);
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.Nota2s.ToListAsync());
+    }
+}
---
+++ E-Vlersimiii/E-Vlersimiii/Controllers/Nota3.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using E_Vlersimiii.Models;
+using Microsoft.EntityFrameworkCore;
+using E_Vlersimiii.Data;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Nota3Controller : ControllerBase
+{
+    private readonly E_VleresimiiContext _context;
+
+    public object?[]? NotaP3 { get; private set; }
+
+    public Nota3Controller(E_VleresimiiContext context)
+    {
+        _context = context;
+    }
+
+    // Get all Nota3
+    [HttpGet("GetNotaP3")]
+    public async Task<ActionResult<List<Nota3>>> Get()
+    {
+        return Ok(await _context.Nota3s.ToListAsync());
+    }
+
+    [HttpGet("{NotaP3}")]
+    // GET: api/Shippingschedules/ID
+
+    public async Task<ActionResult<Nota3>> GetNota1(int NotaP3)
+    {
+        ActionResult<Nota3> Nota3 = await _context.Nota3s.FindAsync(NotaP3);
+
+        if (NotaP3 == null)
+        {
+            return NotFound();
+        }
+
+        return Nota3;
+    }
+
+
+    // Create Nota3
+    [HttpPost("ShtoNota3")]
+    public async Task<ActionResult<List<Nota3>>> AddNota3(Nota3 nota3)
+    {
+        _context.Nota3s.Add(nota3);
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.Nota3s.ToListAsync());
+    }
+
+    // Update Nota3
+    [HttpPut("UpdateNota3")]
+    public async Task<ActionResult<Nota3>> UpdateNota3(Nota3 request)
+    {
+        var dbNota3s = await _context.Nota3s.FindAsync(request.NotaP3);
+        if (dbNota3s == null)
+            return NotFound("Nota3 not found");
+
+        // Your update logic here
+
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.Nota3s.ToListAsync());
+    }
+
+    // Delete a ditar
+    [HttpDelete("FshijeNota3/{NotaP3}")]
+    public async Task<ActionResult<List<Nota3>>> DeleteNota3(int NotaP3)
+    {
+        var dbNota3s = await _context.Nota3s.FindAsync(NotaP3);
+        if (dbNota3s == null)
+            return NotFound("Nota3 not found");
+
+        Nota1 dbNota3 = null;
+        _context.Nota3s.Remove(dbNota3s);
+        await _context.SaveChangesAsync();
+
+        return Ok(await _context.Nota3s.ToListAsync());
+    }
+}
