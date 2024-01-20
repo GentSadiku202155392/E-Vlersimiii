5561908fbdbc03ee7b617d90d9d30eb270316aac
Fluturaibrahimi6 <fi52542@ubt-uni.net>
2024-01-20 17:48:23 +0100

    Models(Lendum,Prindi)

---
+++ E-Vlersimi/E-Vlersimiii/Models/Lendum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E_Vlersimiii.Models
+{
+    public partial class Lendum
+    {
+        public string Lenda { get; set; } = null!;
+    }
+}
